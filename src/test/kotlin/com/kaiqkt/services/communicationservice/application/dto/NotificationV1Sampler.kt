ea17c12d47a8f938eb2347d84babc337246f58dd
kaiqkt <kaiquebeserra1313@gmail.com>
2023-02-09 17:07:46 -0300

    feat: added created data for notifications

--- src/test/kotlin/com/kaiqkt/services/communicationservice/application/dto/NotificationV1Sampler.kt
+++ src/test/kotlin/com/kaiqkt/services/communicationservice/application/dto/NotificationV1Sampler.kt
@@ -1,12 +1,10 @@
 package com.kaiqkt.services.communicationservice.application.dto
 
 import com.kaiqkt.services.communicationservice.generated.application.dto.NotificationV1
-import java.time.LocalDateTime
 
 object NotificationV1Sampler {
     fun sample() = NotificationV1(
         title = "title",
-        body = "body",
-        createdAt = LocalDateTime.now()
+        body = "body"
     )
 }
