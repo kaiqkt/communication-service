df8580e70533be551b6fb08aeb4d8c4a3e8ac838
kaiqkt <kaiquebeserra1313@gmail.com>
2023-04-10 21:17:08 -0300

    feat: date formatter

--- src/main/kotlin/com/kaiqkt/services/communicationservice/application/dto/NotificationV1.kt
+++ src/main/kotlin/com/kaiqkt/services/communicationservice/application/dto/NotificationV1.kt
@@ -5,6 +5,7 @@ import com.kaiqkt.services.communicationservice.domain.entities.Notification
 import com.kaiqkt.services.communicationservice.generated.application.dto.NotificationV1
 
 fun Notification.toV1() = NotificationV1(
+    id = this.id,
     title = this.title,
     body = this.body,
     createdAt = this.createdAt.format(),
